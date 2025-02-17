96a1c2f1f4488135bd27fe89ffa86d0e62d40bfd
LavrentiEmirashvili <lavrentiemirashvili@gmail.com>
2025-02-18 01:10:44 +0400

    update

--- Day 7 - 16.02.25/main.py
+++ Day 7 - 16.02.25/main.py
@@ -1,25 +1,47 @@
-#Step 1 
 import random
+import artstage
+
 
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+end_of_game = False
 chosen_word = random.choice(word_list)
 length = len(chosen_word)
 word = []
-while length > 0:
-    word.insert(1,"_")
-    length -= 1
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Input a letter to guess\n").lower()
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-print(word)
+for i in range(length):
+    word += "_"
 print(chosen_word)
-for letter in chosen_word:
-    if guess == letter:
-        word[letter] = guess
-    elif guess != letter:
-        print("wrong")
+guesses = []
+life = 6
+def check():
+    guess = input("Input a letter to guess\n").lower()
+    
+
+    for i in range(length):
+        if guess in guesses:
+            print('You have already guessed this letter')
+            check()
+            break
+        if guess == chosen_word[i]:
+            word[i] = guess
+            correct_guess = True
+    guesses.append(guess)
+    return correct_guess  
+    
+
+   
+while not end_of_game:
+    if check():
+        print("Correct Guess")
     else:
-        print("wrong input")
-print(word)
+        life -= 1
+    
+    print(f"You have {life} lives left")
+    print(artstage.stages[life])
+    print("".join(word))
+    if '_' not in word:
+        end_of_game = True
+        print("You won!")
+        
+else:
+    print(f'You Lose, The word was: {chosen_word}')
