d7675188e794758c650439d13d93b5dc77014b8a
LavrentiEmirashvili <lavrentiemirashvili@gmail.com>
2025-02-25 02:48:16 +0400

    calc start

---
+++ Day 10 - 25.02.25/calc.py
@@ -0,0 +1,44 @@
+from art import logo
+import os
+print(logo)
+def operate(num1,operation,num2):
+    if operation == "+":
+        answer = num1 + num2 
+    elif operation == "-":
+        answer = num1 - num2 
+    elif operation == "*":
+        answer = num1 * num2 
+    elif operation == "/":
+        if num2 == 0:
+            print("Error: Division by zero is not allowed.")
+            return num1, "n"  
+        answer = num1 / num2 
+    else:
+        print("Invalid operation.")
+        return num1, "n"
+    print(f"{num1} {operation} {num2} = {answer}")
+    continuecalc = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation:")
+    return answer, continuecalc
+
+num1 = float(input("What's the first number?:"))
+operation = input("+\n-\n*\n/\nPick an operation:")
+num2 = float(input("What's the second number?:"))
+
+os.system("cls")
+
+answer, continuecalc = operate(num1, operation, num2)
+
+
+while continuecalc == "y":
+    operation = input("+\n-\n*\n/\nPick an operation: ")
+    num2 = float(input("What's the second number?: "))
+    answer, continuecalc = operate(answer, operation, num2)
+
+os.system("cls")
+
+
+num1 = float(input("What's the first number?: "))
+operation = input("+\n-\n*\n/\nPick an operation: ")
+num2 = float(input("What's the second number?: "))
+
+operate(num1, operation, num2)
