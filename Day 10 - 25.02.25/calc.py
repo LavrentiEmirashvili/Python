b8b0abd1bbb7561316cf2d96d9d58328138bba97
LavrentiEmirashvili <lavrentiemirashvili@gmail.com>
2025-02-26 02:35:02 +0400

    Update calc.py

--- Day 10 - 25.02.25/calc.py
+++ Day 10 - 25.02.25/calc.py
@@ -1,44 +1,83 @@
 from art import logo
 import os
 print(logo)
-def operate(num1,operation,num2):
-    if operation == "+":
-        answer = num1 + num2 
-    elif operation == "-":
-        answer = num1 - num2 
-    elif operation == "*":
-        answer = num1 * num2 
-    elif operation == "/":
-        if num2 == 0:
-            print("Error: Division by zero is not allowed.")
-            return num1, "n"  
-        answer = num1 / num2 
-    else:
-        print("Invalid operation.")
-        return num1, "n"
-    print(f"{num1} {operation} {num2} = {answer}")
-    continuecalc = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation:")
-    return answer, continuecalc
+def addition(a,b):
+    return  a + b
+def substraction(a,b):
+    return  a - b
+def multiplication(a,b):
+    return  a * b
+def division(a,b):
+    return  a / b
 
-num1 = float(input("What's the first number?:"))
-operation = input("+\n-\n*\n/\nPick an operation:")
-num2 = float(input("What's the second number?:"))
+operations = {
+    "+": addition,
+    "-": substraction,
+    "*": multiplication,
+    "/": division
+}
+a = int(input("What's the first number?:"))
+for i in operations:
+    print(i)
+operationChoice = input("Pick an operation:")
+b = int(input("What's the next number?:"))
 
-os.system("cls")
+# for i in operations:
+#     if operationChoice == i:
+#         answer = operations[i](a,b)
 
-answer, continuecalc = operate(num1, operation, num2)
+operation = operations[operationChoice]
+answer1 = operation(a,b)
+
+print(f"{a} {operationChoice} {b} = {answer1}")
+
+continueCalc = input(f"Type 'y' to continue calculating with {answer1}, or type 'n' to exit.:")
+while continueCalc == "y":
+    operationChoice = input("Pick an operation:")
+    operation = operations[operationChoice]
+    b = int(input("What's the next number?:"))
+    answer2 = operation(answer1, b)
+    print(f"{answer1} {operationChoice} {b} = {answer2}")
+    continueCalc = input(f"Type 'y' to continue calculating with {answer2}, or type 'n' to exit.:")
+
+# def operate(num1,operation,num2):
+#     if operation == "+":
+#         answer = num1 + num2 
+#     elif operation == "-":
+#         answer = num1 - num2 
+#     elif operation == "*":
+#         answer = num1 * num2 
+#     elif operation == "/":
+#         if num2 == 0:
+#             print("Error: Division by zero is not allowed.")
+#             return num1, "n"  
+#         answer = num1 / num2 
+#     else:
+#         print("Invalid operation.")
+#         return num1, "n"
+#     print(f"{num1} {operation} {num2} = {answer}")
+#     continuecalc = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation:")
+#     return answer, continuecalc
+
+# num1 = float(input("What's the first number?:"))
+# operation = input("+\n-\n*\n/\nPick an operation:")
+# num2 = float(input("What's the second number?:"))
+
+# os.system("cls")
+
+# answer, continuecalc = operate(num1, operation, num2)
 
 
-while continuecalc == "y":
-    operation = input("+\n-\n*\n/\nPick an operation: ")
-    num2 = float(input("What's the second number?: "))
-    answer, continuecalc = operate(answer, operation, num2)
+# while continuecalc == "y":
+#     operation = input("+\n-\n*\n/\nPick an operation: ")
+#     num2 = float(input("What's the second number?: "))
+#     answer, continuecalc = operate(answer, operation, num2)
 
-os.system("cls")
+# os.system("cls")
 
 
-num1 = float(input("What's the first number?: "))
-operation = input("+\n-\n*\n/\nPick an operation: ")
-num2 = float(input("What's the second number?: "))
+# num1 = float(input("What's the first number?: "))
+# operation = input("+\n-\n*\n/\nPick an operation: ")
+# num2 = float(input("What's the second number?: "))
 
-operate(num1, operation, num2)
+# operate(num1, operation, num2)
