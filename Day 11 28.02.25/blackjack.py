ff5b03230270de19f437aaa9e68ae85b447fa8f0
LavrentiEmirashvili <lavrentiemirashvili@gmail.com>
2025-03-01 03:13:11 +0400

    updated blackjack

--- Day 11 28.02.25/blackjack.py
+++ Day 11 28.02.25/blackjack.py
@@ -11,8 +11,8 @@
 ## There are no jokers. 
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# Use the following list as the deck of cards:
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
@@ -23,8 +23,11 @@
 #   https://games.washingtonpost.com/games/blackjack/
 #Then try out the completed Blackjack project here: 
 #   https://appbrewery.github.io/python-day11-demo/
-
+import random 
 from art import logo
 print(logo)
 
-def draw_cards()
+def draw_cards():
+    cardDrawn = random.choice(cards)
+    print(cardDrawn) 
+draw_cards()
