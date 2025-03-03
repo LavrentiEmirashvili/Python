5cc0614562d9375d96c89dc8bbe3e5ddee6cdf71
LavrentiEmirashvili <lavrentiemirashvili@gmail.com>
2025-03-04 01:45:13 +0400

    higher lower game finished

--- Day 14 - 02.03.25/higher_lower_game.py
+++ Day 14 - 02.03.25/higher_lower_game.py
@@ -3,15 +3,53 @@ import os
 import art
 from game_data import data
 
-print(art.logo)
 
-a = random.choice(data)
-b = random.choice(data)
+def comparison(a, b):
+    os.system("cls")
+    print(art.logo)
+    
+    if score > 0:
+        print(F"You're right! Current score: {score}.")
 
-print(f"Compare A: {a["name"]}, a {a["description"]}, from {a["country"]}")
+    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']}")
 
-print(art.vs)
+    print(art.vs)
 
-print(f"Against B: {b["name"]}, a {b["description"]}, from {b["country"]}")
+    print(f"Against B: {b['name']}, a {b['description']}, from {b['country']}")
 
-choice = input("Who has more followers? Type 'A' or 'B': ")
+
+def HLGame():
+    global score
+    score = 0
+    a = random.choice(data)
+
+    while True:
+        b = random.choice(data)
+        while b == a:
+            b = random.choice(data)
+
+        comparison(a,b)
+
+        choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+        
+
+        if a["follower_count"] > b["follower_count"]:
+            correctChoice = "a"
+        elif a["follower_count"] == b["follower_count"]:
+            correctChoice = "tie"
+        else:
+            correctChoice = "b"
+
+        if choice == correctChoice or correctChoice == "tie":
+            score += 1
+            if "a" == correctChoice:
+                a = a
+            else:
+                a = b
+        else:
+            os.system("cls")
+            print(art.logo)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            break
+
+HLGame()
